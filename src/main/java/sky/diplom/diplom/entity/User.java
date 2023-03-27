1c7bd3e298ad47e1af0a15a29fe986170c6d8df1
ViharevN <swix_off@mail.ru>
2023-03-28 01:17:29 +0300

    add class User, UserImage

---
+++ src/main/java/sky/diplom/diplom/entity/User.java
@@ -0,0 +1,31 @@
+package sky.diplom.diplom.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "user_name")
+    private String userName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "password")
+    private String password;
+    @OneToOne
+    private UserImage userImage;
+
+}
