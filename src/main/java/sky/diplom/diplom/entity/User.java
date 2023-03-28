a587f2072026fe9ed02e0e7f903fe7be1d26cb85
ViharevN <swix_off@mail.ru>
2023-03-28 15:19:25 +0300

    commit

--- src/main/java/sky/diplom/diplom/entity/User.java
+++ src/main/java/sky/diplom/diplom/entity/User.java
@@ -16,7 +16,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "user_name")
-    private String userName;
+    private String username;
+    @Column(name = "first_name")
+    private String firstName;
     @Column(name = "last_name")
     private String lastName;
     @Column(name = "email")
