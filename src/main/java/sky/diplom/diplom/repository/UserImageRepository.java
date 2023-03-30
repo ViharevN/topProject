a674636f27789137a9fd5e89ab63f2f29a01bfd8
ViharevN <swix_off@mail.ru>
2023-03-31 00:29:25 +0300

    add LoginReqMapper, @Repository

--- src/main/java/sky/diplom/diplom/repository/UserImageRepository.java
+++ src/main/java/sky/diplom/diplom/repository/UserImageRepository.java
@@ -1,7 +1,9 @@
 package sky.diplom.diplom.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import sky.diplom.diplom.entity.UserImage;
 
+@Repository
 public interface UserImageRepository extends JpaRepository<UserImage, Long> {
 }
--- src/main/java/sky/diplom/diplom/repository/UserRepository.java
+++ src/main/java/sky/diplom/diplom/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package sky.diplom.diplom.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import sky.diplom.diplom.entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
