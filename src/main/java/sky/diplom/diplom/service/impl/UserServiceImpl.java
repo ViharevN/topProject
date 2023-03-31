847e0ec05e1e50f7e8159c316f30170e7821b694
ViharevN <swix_off@mail.ru>
2023-03-31 11:52:01 +0300

    commit

--- src/main/java/sky/diplom/diplom/service/impl/UserServiceImpl.java
+++ src/main/java/sky/diplom/diplom/service/impl/UserServiceImpl.java
@@ -1,7 +1,6 @@
 package sky.diplom.diplom.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import sky.diplom.diplom.repository.UserRepository;
 import sky.diplom.diplom.service.UserService;
@@ -10,7 +9,7 @@ import sky.diplom.diplom.service.UserService;
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository repository;
-    private final PasswordEncoder passwordEncoder;
-    private final UserDetailServiceImpl userDetailService;
+
+
 
 }
