30c2a29c6045e6b74650cfedb5581cfbc9f3d80b
ViharevN <swix_off@mail.ru>
2023-04-07 02:54:35 +0300

    add security

--- src/main/java/sky/diplom/diplom/security/MyUserDetails.java
+++ src/main/java/sky/diplom/diplom/security/MyUserDetails.java
@@ -1,2 +1,22 @@
-package sky.diplom.diplom.security;public class MyUserDetails {
+package sky.diplom.diplom.security;
+
+
+import lombok.Getter;
+import sky.diplom.diplom.entity.User;
+
+import java.util.List;
+
+@Getter
+public class MyUserDetails extends org.springframework.security.core.userdetails.User {
+
+    private final Long id;
+
+    public MyUserDetails(User user) {
+        super(user.getEmail(), user.getPassword(), List.of(user.getRole()));
+        this.id = user.getId();
+    }
+
+    @Override
+    public void eraseCredentials() {
+    }
 }
--- src/main/java/sky/diplom/diplom/security/SecurityUtils.java
+++ src/main/java/sky/diplom/diplom/security/SecurityUtils.java
@@ -1,4 +1,36 @@
 package sky.diplom.diplom.security;
 
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import sky.diplom.diplom.dto.Role;
+import sky.diplom.diplom.entity.Ads;
+import sky.diplom.diplom.entity.Comment;
+
+import java.nio.file.AccessDeniedException;
+
+@NoArgsConstructor
 public class SecurityUtils {
+    public static MyUserDetails getUserDetailsFromContext() {
+        return (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+    public static long getUserIdFromContext() {
+        return getUserDetailsFromContext().getId();
+    }
+
+    public static void checkPermissionToAds(Ads ads) throws AccessDeniedException {
+        MyUserDetails userDetails = getUserDetailsFromContext();
+        if (!userDetails.getAuthorities().contains(Role.ADMIN) && userDetails.getId() != ads.getAuthor().getId()) {
+            throw new AccessDeniedException("Чтобы изменить или удалить " +
+                    "объявление необходимо иметь роль Admin, " +
+                    "или являться автором этого объявления");
+        }
+    }
+
+    public static void checkPermissionToComments(Comment comment) {
+        MyUserDetails userDetails = getUserDetailsFromContext();
+        if (!userDetails.getAuthorities().contains(Role.ADMIN) && userDetails.getId() != comment.getPk()) {
+
+        }
+    }
 }
